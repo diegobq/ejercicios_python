203266815c30105c89f3e4013e57cd08cf1ae8c3
diegobq <diegobq@gmail.com>
2021-09-27 20:56:53 -0300

    arregle el ejercicio 7.12

--- Clase07/random_walk.py
+++ Clase07/random_walk.py
@@ -22,32 +22,32 @@ def randomwalk(largo):
     pasos = np.random.randint(-1,2,largo)
     return pasos.cumsum()
 
-def main():
-    num_trayectorias = 12
-    N = 100000
+def graficar_caminatas(trayectorias, trayectoria_mayor, trayectoria_menor):
+    '''
+    Grafica las diferentes trayectorias generadas
+    Y además grafica la trayectoria que más y y que menos se alejan
 
+    Parameters
+    ----------
+    trayectorias : TYPE
+        DESCRIPTION.
+    trayectoria_mayor : TYPE
+        DESCRIPTION.
+    trayectoria_menor : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    '''
+    num_trayectorias = len(trayectorias)
     figure = plt.figure()
 
     ax = figure.add_subplot(2, 1, 1)
     ax.set_title(f'{num_trayectorias} Caminatas al azar', {'fontsize': 10})
 
-    max_distancia = None
-    trayectoria_mayor = []
-
-    min_distancia = None
-    trayectoria_menor = []
-
-    for i in range(num_trayectorias):
-        trayectoria = randomwalk(N)
-
-        distancia = trayectoria[-1]
-        if max_distancia == None or distancia > max_distancia:
-            max_distancia = distancia
-            trayectoria_mayor = trayectoria
-        if min_distancia == None or distancia < min_distancia:
-            min_distancia = distancia
-            trayectoria_menor = trayectoria
-
+    for trayectoria in trayectorias:
         ax.plot(trayectoria)
 
     ax1 = figure.add_subplot(2, 2, 3)
@@ -63,6 +63,32 @@ def main():
     figure.text(0.06, 0.5, 'distancia al origen', ha='center', va='center', rotation='vertical')
 
     plt.show()
+
+def main():
+    num_trayectorias = 12
+    N = 100000
+    trayectorias = []
+
+    max_distancia = None
+    trayectoria_mayor = []
+
+    min_distancia = None
+    trayectoria_menor = []
+
+    for i in range(num_trayectorias):
+        trayectoria = randomwalk(N)
+
+        distancia = max(abs(trayectoria))
+        if max_distancia == None or distancia > max_distancia:
+            max_distancia = distancia
+            trayectoria_mayor = trayectoria
+        if min_distancia == None or distancia < min_distancia:
+            min_distancia = distancia
+            trayectoria_menor = trayectoria
+
+        trayectorias.append(trayectoria)
+
+    graficar_caminatas(trayectorias, trayectoria_mayor, trayectoria_menor)
 
 main()
 
